e40db0a7bbadacffae93098232d0c4c6d1962ae6
Neville <neville.lewis@>
2019-10-20 21:52:44 -0600

    specified the version of powershell to use by full path

--- Install-website.ps1
+++ Install-website.ps1
@@ -6,17 +6,17 @@ Set-Location C:\website-dropfolder
 
 # Create an IIS website and point it to the published folder
 #New-WebSite -Name CoreWebsite -Port 80 -HostHeader CoreWebsite -PhysicalPath "$env:systemdrive\website-published"  
-powershell.exe -Command {Import-Module WebAdministration; New-WebSite -Name CoreWebsite -Port 80 -HostHeader CoreWebsite -PhysicalPath "$env:systemdrive\website-published"}
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; New-WebSite -Name CoreWebsite -Port 80 -HostHeader CoreWebsite -PhysicalPath "$env:systemdrive\website-published"}
 
 # Create AppPool for website and set CLR version to core-dotnet
 #New-WebAppPool CoreWebsiteAppPool  -ErrorAction SilentlyContinue
 #Set-ItemProperty IIS:\\AppPools\CoreWebsiteAppPool managedRuntimeVersion "" -verbose  
-powershell.exe -Command {Import-Module WebAdministration; New-WebAppPool CoreWebsiteAppPool}
-powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty IIS:\\AppPools\CoreWebsiteAppPool managedRuntimeVersion "" -verbose}
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; New-WebAppPool CoreWebsiteAppPool}
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty IIS:\\AppPools\CoreWebsiteAppPool managedRuntimeVersion "" -verbose}
 
 # Set the app-pool of the website
 #Set-ItemProperty 'IIS:\Sites\CoreWebsite' applicationPool CoreWebsiteAppPool
-powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\Sites\CoreWebsite' applicationPool CoreWebsiteAppPool}
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\Sites\CoreWebsite' applicationPool CoreWebsiteAppPool}
 
 # Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
 # To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
