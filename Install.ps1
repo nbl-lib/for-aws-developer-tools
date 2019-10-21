5c60530ad5b52fcdb4c301a7ebb40ca9705721b3
Neville <neville.lewis@>
2019-10-20 22:34:04 -0600

    cleanup of install.ps1

--- Install.ps1
+++ Install.ps1
@@ -48,6 +48,3 @@ C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Modul
 #Set-ItemProperty 'IIS:\Sites\CoreWebsite' applicationPool CoreWebsiteAppPool
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\Sites\CoreWebsite' applicationPool CoreWebsiteAppPool}
 
-# Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
-# To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
-##C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\ExploringAspNetCore\publish}
