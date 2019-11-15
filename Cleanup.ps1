e830a9cb86ac5ad8d5cf9bd67e99b6f358413c01
Neville <neville.lewis@>
2019-11-15 10:08:56 -0700

    added entry to stop website to delete all files

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -1,1 +1,8 @@
+#stop  the website
+Stop-WebSite -Name "CoreWebsite"
+
+#remove old files
 Remove-Item C:\website-published -Recurse -ErrorAction Ignore
+
+#Start the website once clean up done
+Start-WebSite -Name "CoreWebsite"
